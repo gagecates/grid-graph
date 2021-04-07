45f8853cd23ec4ea16260137e17572b002ff6703
Gage Cates <gagecates10@gmail.com>
2021-04-07 17:07:00 -0600

    fix server path and axios request path

--- back-end/graph.py
+++ back-end/graph.py
@@ -209,8 +209,6 @@ def build_user_graph():
         })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="5000", debug=False)
-
-# port=os.environ.get('PORT', 80)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
 
